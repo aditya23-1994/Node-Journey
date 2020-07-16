4df4b1fbe505c45c62c6241405950c9b3d88dd0a
aditya23-1994 <adityakhatwa@outlook.com>
2020-07-16 21:17:41 +0530

    contains express and mvc structure of node without database

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-
 const  adminRoutes = require('./routes/admin');
 const  shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
+app.use(express.static(path.join(__dirname,'public')));
 
-app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page not found!</h1>');
-});
+app.use(errorController.get404);
 
 app.listen(3000);
